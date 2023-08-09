ed64022918a689257f7d743a1e79b942f77db79d
Eryk J <107011362+erykjj@users.noreply.github.com>
2023-08-09 19:15:18 -0400

    Process config in one function

--- implexus.py
+++ implexus.py
@@ -40,58 +40,47 @@ def sh(command, arguments='', inp=''):
         exit()
     return res.stdout.decode('utf-8')
 
-# {'mesh': {'name': 'mesh', 'keep_alive': 25}, 'devices': [{'name': 'acer', 'mesh_ip': '10.2.2.1', 'real_ip': 'personalis.duckdns.org', 'port': 51821, 'connect_to': ['rpi', 'asus', 'vm', 'mom', 'dad']}, {'name': 'rpi', 'mesh_ip': '10.2.2.2', 'real_ip': '03.crabdance.com', 'port': 51822, 'connect_to': ['mom', 'dad']}, {'name': 'asus', 'mesh_ip': '10.2.2.3', 'real_ip': 'laboris.duckdns.org', 'port': 51823, 'connect_to': ['acer', 'rpi', 'vm', 'mom', 'dad']}, {'name': 'vm', 'mesh_ip': '10.2.2.4', 'real_ip': '03.jumpingcrab.com', 'port': 51824}, {'name': 'mom', 'mesh_ip': '10.2.2.5', 'real_ip': '02.crabdance.com', 'port': 51821, 'connect_to': ['dad']}, {'name': 'dad', 'mesh_ip': '10.2.2.6', 'real_ip': '01.jumpingcrab.com', 'port': 51822, 'connect_to': ['mom']}, {'name': 'x3', 'mesh_ip': '10.2.2.7', 'connect_to': ['acer', 'rpi', 'asus', 'vm', 'mom', 'dad']}, {'name': 'p605', 'mesh_ip': '10.2.2.8', 'connect_to': ['acer', 'rpi', 'asus', 'vm', 'mom', 'dad']}]}
-
-def read_yaml(config):
+def process_config(config):
     with open(config) as f:
-        data = yaml.load(f, Loader=yaml.loader.SafeLoader)
-    mesh = {}
-    mesh['Network'] = {
-        'Name': data['mesh']['name'],
-        # 'Range': data['mesh']['ip_range'],
-        'PersistentKeepalive': data['mesh']['keep_alive'] }
-    for device in data['devices']:
-        private_key = sh('wg', 'genkey').rstrip('\n')
-        public_key = sh('wg', 'pubkey', private_key).rstrip('\n')
-        mesh[device['name']] = {
-            'Address': device['mesh_ip'],
-            'PrivateKey': private_key,
-            'PublicKey': public_key }
-        if 'real_ip' in device.keys():
-            mesh[device['name']]['Endpoint'] = device['real_ip']
-        if 'port' in device.keys():
-            mesh[device['name']]['ListenPort'] = device['port']
-        if 'connect_to' in device.keys():
-            mesh[device['name']]['Peers'] = device['connect_to']
-    return mesh
+        mesh = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
-def output_configs(mesh):
-    output_dir = OUTPUT_DIR + '/' + mesh['Network']['Name']
+    output_dir = OUTPUT_DIR + '/' + mesh['NetworkName']
     os.makedirs(output_dir, exist_ok=True)
-    for device in mesh:
-        if device == 'Network':
+
+    for device in mesh.keys():
+        if device == 'NetworkName':
             continue
         os.makedirs(output_dir + '/' + device, exist_ok=True)
-        conf = f"[Interface]\n# Name: {device}\nAddress = {mesh[device]['Address']}/24\nPrivateKey = {mesh[device]['PrivateKey']}"
-        if 'ListenPort' in mesh[device].keys():
-            conf += f"\nListenPort = {mesh[device]['ListenPort']}"
-        if 'Peers' in mesh[device].keys():
-            for peer in mesh[device]['Peers']:
-                conf += f"\n\n[Peer]\n# Name: {peer}\nPublicKey = {mesh[peer]['PublicKey']}\nEndpoint = {mesh[peer]['Endpoint']}"
-                if 'ListenPort' in mesh[peer].keys():
-                    conf += f":{mesh[peer]['ListenPort']}"
-                conf += f"\nAllowedIPs = {mesh[peer]['Address']}\nPersistentKeepalive = {mesh['Network']['PersistentKeepalive']}"
-        file_name = f"{output_dir}/{device}/wg-{mesh['Network']['Name']}.conf"
+        if 'PrivateKey' not in mesh[device].keys():
+            mesh[device]['PrivateKey'] = sh('wg', 'genkey').rstrip('\n')
+            mesh[device]['PublicKey'] = sh('wg', 'pubkey', mesh[device]['PrivateKey']).rstrip('\n')
+        if 'ListenPort' not in mesh[device].keys():
+            mesh[device]['ListenPort'] = 51820
+
+    for device in mesh.keys():
+        if device == 'NetworkName':
+            continue
+        conf = f"[Interface]\n# Name: {device}\nAddress = {mesh[device]['Address']}/24\nPrivateKey = {mesh[device]['PrivateKey']}\nListenPort = {mesh[device]['ListenPort']}"
+
+        for peer in mesh.keys():
+            if peer == 'NetworkName' or peer == device:
+                continue
+            conf += f"\n\n[Peer]\n# Name: {peer}\nPublicKey = {mesh[peer]['PublicKey']}"
+            if 'Endpoint' in mesh[peer].keys():
+                conf += f"\nEndpoint = {mesh[peer]['Endpoint']}:{mesh[peer]['ListenPort']}"
+            conf += f"\nAllowedIPs = {mesh[peer]['Address']}\nPersistentKeepalive = 25"
+
+        file_name = f"{output_dir}/{device}/wg-{mesh['NetworkName']}.conf"
         with open(file_name, 'w', encoding='UTF-8') as f:
             f.write(conf)
         print(f'Generated {file_name}')
-    with open(f"{output_dir}/{mesh['Network']['Name']}.yaml", 'w', encoding='UTF-8') as f:
-        yaml.dump(mesh, f, Dumper=yaml.dumper.SafeDumper)
+    # with open(f"{output_dir}/{mesh['NetworkName']}.yaml", 'w', encoding='UTF-8') as f:
+    #     yaml.dump(mesh, f, Dumper=yaml.dumper.SafeDumper, indent=4)
 
 # if __name__ == "__main__":
     # PROJECT_PATH = Path(__file__).resolve().parent
     # APP = Path(__file__).stem
 
 OUTPUT_DIR = '/mnt/ramdisk'
-mesh = read_yaml('network.yaml')
-output_configs(mesh)
+process_config('config.yaml')
+
